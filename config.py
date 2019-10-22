c3c74c5bd6cec529c189237c5d2ab7e7e49fe605
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-10-22 18:07:57 +0100

    Add default values to config params

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from prettyconf import config
 
-PLC_URL = config('PLC_URL')
-PLC_USERNAME = config('PLC_USERNAME')
-PLC_PASSWORD = config('PLC_PASSWORD')
+PLC_URL = config('PLC_URL', default='127.0.0.1')
+PLC_USERNAME = config('PLC_USERNAME', default='admin')
+PLC_PASSWORD = config('PLC_PASSWORD', default='1234')
